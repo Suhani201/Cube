184e1cf75126be0697869e938460e9a1f82c0ed9
Suhani <01fe24ica041@kletech.ac.in>
2026-01-15 03:14:12 +0530

    cube is

--- cube.py
+++ cube.py
@@ -1,11 +1,3 @@
-# cube.py
-
-def find_cube(num):
-    """Return cube of a number."""
-    return num * num * num
-
-
-if __name__ == "__main__":
-    num = float(input("Enter a number: "))
-    cube = find_cube(num)
-    print("Cube of the number is:", cube)
+num = 2 
+cube = num ** 3 
+print("Cube of the number:", cube) 
